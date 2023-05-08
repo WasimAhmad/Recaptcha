0ac5089726337b8f50784675dfd954b84babad01
Waseem Ahmad Mughal <WasimAhmad@users.noreply.github.com>
2023-05-08 11:58:53 +0500

    Add project files.

---
+++ Recaptcha/CustomCaptcha.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
+using System.IO;
+
+using Org.BouncyCastle.Crypto.Digests;
+
+
+namespace Recaptcha
+{
+
+    public class CustomCaptcha
+    {
+        public string CaptchaImageFilePath { get; private set; }
+        public string AnswerHash { get; private set; }
+
+        public CustomCaptcha()
+        {
+            GenerateCaptcha();
+        }
+
+        private void GenerateCaptcha()
+        {
+            int width = 200;
+            int height = 100;
+
+            var random = new Random();
+            var font = new Font(FontFamily.GenericSansSerif, 40, FontStyle.Bold, GraphicsUnit.Pixel);
+            var text = GenerateRandomText(6);
+
+            using (var bmp = new Bitmap(width, height))
+            using (var gfx = Graphics.FromImage(bmp))
+            {
+                gfx.Clear(Color.White);
+
+                // Draw text
+                var textSize = gfx.MeasureString(text, font);
+                var x = (width - textSize.Width) / 2;
+                var y = (height - textSize.Height) / 2;
+                gfx.DrawString(text, font, Brushes.Black, x, y);
+
+                // Add noise
+                for (int i = 0; i < 50; i++)
+                {
+                    var pen = new Pen(Color.FromArgb(random.Next(256), random.Next(256), random.Next(256)));
+                    gfx.DrawLine(pen, random.Next(width), random.Next(height), random.Next(width), random.Next(height));
+                }
+
+                // Save the image to a file
+                CaptchaImageFilePath = "captcha.png";
+                bmp.Save(CaptchaImageFilePath, ImageFormat.Png);
+            }
+
+            // Calculate and store the answer hash
+            AnswerHash = CalculateSha256(text);
+        }
+
+        private string GenerateRandomText(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var random = new Random();
+            var result = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = chars[random.Next(chars.Length)];
+            }
+
+            return new string(result);
+        }
+
+        public string CalculateSha256(string input)
+        {
+            var digest = new Sha256Digest();
+            var bytes = System.Text.Encoding.UTF8.GetBytes(input);
+            digest.BlockUpdate(bytes, 0, bytes.Length);
+            var result = new byte[digest.GetDigestSize()];
+            digest.DoFinal(result, 0);
+            return BitConverter.ToString(result).Replace("-", "").ToLowerInvariant();
+        }
+    }
+
+}
---
+++ Recaptcha/Program.cs
@@ -0,0 +1,26 @@
+﻿using Recaptcha;
+
+using System.Drawing.Text;
+
+var captcha = new CustomCaptcha();
+
+// Display the CAPTCHA image to the user (e.g., in a Windows Forms or WPF application)
+Console.WriteLine($"Please open the CAPTCHA image from the following path: {captcha.CaptchaImageFilePath}");
+
+// Get user input
+Console.Write("Enter the text from the CAPTCHA image: ");
+
+var userInput = Console.ReadLine();
+
+// Calculate the hash of the user's input
+var inputHash = captcha.CalculateSha256(userInput);
+
+// Verify the user's input
+if (inputHash == captcha.AnswerHash)
+{
+    Console.WriteLine("Success! You entered the correct CAPTCHA.");
+}
+else
+{
+    Console.WriteLine("Incorrect CAPTCHA.");
+}
